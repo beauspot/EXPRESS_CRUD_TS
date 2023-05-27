3d96f27b296fcbd0feb3f6a85193f062f49694ca
beauspot <beauspot@outlook.com>
2023-05-27 18:44:19 +0100

    finished setting up and testing the task controllers for this application.

--- build/controllers/taskCtrls.js
+++ build/controllers/taskCtrls.js
@@ -12,20 +12,51 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTask = exports.getAllTasks = void 0;
+exports.deleteATask = exports.updateASingleTask = exports.getASingleTask = exports.createTask = exports.getAllTasks = void 0;
 const http_status_codes_1 = require("http-status-codes");
 const userPosts_1 = require("../models/userPosts");
 const express_async_handler_1 = __importDefault(require("express-async-handler"));
-// getting all tasks 
+const logger_1 = __importDefault(require("../library/logger"));
+// getting all tasks
 exports.getAllTasks = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const tasks = yield userPosts_1.UserPostModel.find();
-    console.log(tasks);
+    logger_1.default.log(tasks);
     res.status(http_status_codes_1.StatusCodes.OK).json(tasks);
 }));
 // creating a post task controller
 exports.createTask = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const createTask = yield userPosts_1.UserPostModel.create(req.body);
     const savedTask = yield createTask.save();
-    console.log(savedTask);
+    logger_1.default.log(savedTask);
     res.status(http_status_codes_1.StatusCodes.CREATED).json({ msg: 'Task posted successfully', data: savedTask });
+}));
+// getting a single task from the DB
+exports.getASingleTask = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id: taskID } = req.params;
+    const singleTask = yield userPosts_1.UserPostModel.findOne({ _id: taskID });
+    logger_1.default.log(singleTask);
+    if (!singleTask)
+        res.status(http_status_codes_1.StatusCodes.BAD_REQUEST).json({ msg: `Task with ${taskID} does not exist` });
+    else
+        res.status(http_status_codes_1.StatusCodes.OK).json({ status: http_status_codes_1.StatusCodes.OK, taskData: singleTask });
+}));
+// updating a single task from the DB
+exports.updateASingleTask = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id: taskID } = req.params;
+    const updateTask = yield userPosts_1.UserPostModel.findOneAndUpdate({ _id: taskID }, req.body, { new: true, runValidators: true });
+    logger_1.default.log(updateTask);
+    if (!updateTask)
+        res.status(http_status_codes_1.StatusCodes.BAD_REQUEST).json({ msg: `Task with ${taskID} does not exist` });
+    else
+        res.status(http_status_codes_1.StatusCodes.OK).json({ status: http_status_codes_1.StatusCodes.OK, taskData: updateTask });
+}));
+// deleting a single task from the database
+exports.deleteATask = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id: taskID } = req.params;
+    const deleteTask = yield userPosts_1.UserPostModel.findOneAndDelete({ _id: taskID });
+    logger_1.default.warn(deleteTask);
+    if (!deleteTask)
+        res.status(http_status_codes_1.StatusCodes.BAD_REQUEST).json({ msg: `Task with ${taskID} does not exist` });
+    else
+        res.status(http_status_codes_1.StatusCodes.OK).json({ status: http_status_codes_1.StatusCodes.OK, taskData: deleteTask });
 }));
--- src/controllers/taskCtrls.ts
+++ src/controllers/taskCtrls.ts
@@ -2,19 +2,46 @@ import { StatusCodes } from 'http-status-codes';
 import { UserPostModel } from '../models/userPosts';
 import asyncHandler from 'express-async-handler';
 import { Request, Response, NextFunction } from 'express';
+import Logger from '../library/logger';
 
-// getting all tasks 
+// getting all tasks
 export const getAllTasks = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const tasks = await UserPostModel.find();
-    console.log(tasks);
+    Logger.log(tasks);
     res.status(StatusCodes.OK).json(tasks);
- })
+});
 
 // creating a post task controller
 export const createTask = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const createTask = await UserPostModel.create(req.body);
     const savedTask = await createTask.save();
-    console.log(savedTask);
+    Logger.log(savedTask);
     res.status(StatusCodes.CREATED).json({ msg: 'Task posted successfully', data: savedTask });
 });
 
+// getting a single task from the DB
+export const getASingleTask = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { id: taskID } = req.params;
+    const singleTask = await UserPostModel.findOne({ _id: taskID });
+    Logger.log(singleTask);
+    if (!singleTask) res.status(StatusCodes.BAD_REQUEST).json({ msg: `Task with ${taskID} does not exist` });
+    else res.status(StatusCodes.OK).json({ status: StatusCodes.OK, taskData: singleTask });
+});
+
+// updating a single task from the DB
+export const updateASingleTask = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { id: taskID } = req.params;
+    const updateTask = await UserPostModel.findOneAndUpdate({ _id: taskID }, req.body, { new: true, runValidators: true });
+    Logger.log(updateTask);
+    if (!updateTask) res.status(StatusCodes.BAD_REQUEST).json({ msg: `Task with ${taskID} does not exist` });
+    else res.status(StatusCodes.OK).json({ status: StatusCodes.OK, taskData: updateTask });
+});
+
+// deleting a single task from the database
+export const deleteATask = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { id: taskID } = req.params;
+    const deleteTask = await UserPostModel.findOneAndDelete({ _id: taskID });
+    Logger.warn(deleteTask);
+    if (!deleteTask) res.status(StatusCodes.BAD_REQUEST).json({ msg: `Task with ${taskID} does not exist` });
+    else res.status(StatusCodes.OK).json({ status: StatusCodes.OK, taskData: deleteTask });
+});
