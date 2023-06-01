0da3c837852aa4468288c30ab9846e3b8076c175
beauspot <beauspot@outlook.com>
2023-06-01 19:18:45 +0100

    the compiled file to js of the authentication middleware.

---
+++ build/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.auth = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const unauthenticated_1 = __importDefault(require("../errors/unauthenticated"));
+const http_status_codes_1 = require("http-status-codes");
+const express_async_handler_1 = __importDefault(require("express-async-handler"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+exports.auth = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    // check header or url parameters or post parameters for token
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer '))
+        throw new unauthenticated_1.default('User authentication failed.', http_status_codes_1.StatusCodes.UNAUTHORIZED);
+    const token = authHeader.split(' ')[1];
+    const payload = jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET);
+    console.log(payload);
+    // attach the user request object
+    req.user = {
+        userId: payload.userId,
+        name: payload.name,
+        role: payload.role
+    };
+    next();
+}));
