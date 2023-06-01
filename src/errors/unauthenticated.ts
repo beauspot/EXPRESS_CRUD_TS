870d06f982fa34df00fa9e59ab203900dfe29474
beauspot <beauspot@outlook.com>
2023-06-01 19:16:33 +0100

    Created the unauthenticated middleware for handling user authentication error.

---
+++ src/errors/unauthenticated.ts
@@ -0,0 +1,10 @@
+// import { StatusCodes } from 'http-status-codes';
+import CustomAPIError from './custom-errors';
+
+class UnauthenticatedError extends CustomAPIError {
+    constructor(public message: string, public statusCode: number) {
+        super(message, statusCode);
+    }
+}
+
+export default UnauthenticatedError;
