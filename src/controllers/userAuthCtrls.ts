98ef2ad89f544e08bcc6b492f27c76d7a5cf8067
beauspot <beauspot@outlook.com>
2023-06-01 09:59:25 +0100

    fixed the issue of null value on the user authentication module.

--- src/controllers/userAuthCtrls.ts
+++ src/controllers/userAuthCtrls.ts
@@ -2,15 +2,16 @@ import dotenv from 'dotenv';
 import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 import { Request, Response, NextFunction } from 'express';
-import { UserAuthModel } from '../models/userAuth';
+import { UserAuthModel, UserAuthentication } from '../models/userAuth';
 dotenv.config();
 
 // user signup controller
 export const SignUpUser = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const newUser = await UserAuthModel.create({ ...req.body });
-    const userToken = newUser.generateAuthToken();
-
+    const userToken = await newUser.generateAuthToken();
+    console.log(userToken);
     res.status(StatusCodes.CREATED).json({ UserData: { username: newUser.username, token: userToken } });
+    return;
 });
 
 // user Login Controller
@@ -18,20 +19,22 @@ export const LoginUser = asyncHandler(async (req: Request, res: Response, next: 
     const { email, password } = req.body;
 
     if (!email || !password) {
-        res.status(StatusCodes.BAD_REQUEST).json({ errMessage: `All Fields Are Mandatory` })
+        res.status(StatusCodes.BAD_REQUEST).json({ errMessage: `All Fields Are Mandatory` });
         return;
     }
 
-    const UserExists = await UserAuthModel.findOne({ email });
+    const UserExists = (await UserAuthModel.findOne({ email })) as UserAuthentication;
 
-    if (UserExists) {
+    if (!UserExists) {
         res.status(StatusCodes.UNAUTHORIZED).json({
             errMessage: `The user with the email: ${email} is not registered`
         });
         return;
     }
     // comparing user password
-    const isMatch = await UserAuthModel.validPassword(password, UserExists.password);
+
+    const isMatch: boolean = await UserExists.comparePassword(password);
+    console.log(isMatch); // this should return a boolean value.
 
     if (!isMatch) {
         res.status(StatusCodes.UNAUTHORIZED).json({
@@ -43,5 +46,4 @@ export const LoginUser = asyncHandler(async (req: Request, res: Response, next: 
     const userToken = UserExists.generateAuthToken();
 
     res.status(StatusCodes.OK).json({ UserData: { username: UserExists.username, token: userToken } });
-   
 });
