69aa17c436d7b05cc0911c3cc6d22c1b1be5870a
beauspot <beauspot@outlook.com>
2023-06-08 18:23:51 +0100

    this is just the compiled version of the middleware for authentication.

--- build/middleware/authMiddleware.js
+++ build/middleware/authMiddleware.js
@@ -12,26 +12,38 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.auth = void 0;
+exports.authMiddleware = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const unauthenticated_1 = __importDefault(require("../errors/unauthenticated"));
 const http_status_codes_1 = require("http-status-codes");
 const express_async_handler_1 = __importDefault(require("express-async-handler"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-exports.auth = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+/* interface AuthenticatedRequest extends Request {
+    user: {
+        userId: string;
+        name: string;
+        role: string;
+    };
+} */
+exports.authMiddleware = (0, express_async_handler_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     // check header or url parameters or post parameters for token
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Bearer '))
         throw new unauthenticated_1.default('User authentication failed.', http_status_codes_1.StatusCodes.UNAUTHORIZED);
     const token = authHeader.split(' ')[1];
-    const payload = jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET);
-    console.log(payload);
-    // attach the user request object
-    req.user = {
-        userId: payload.userId,
-        name: payload.name,
-        role: payload.role
-    };
-    next();
+    try {
+        const payload = jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET);
+        console.log(payload);
+        // attach the user request object
+        req.user = {
+            userId: payload.userId,
+            name: payload.name,
+            role: payload.role
+        };
+        next();
+    }
+    catch (error) {
+        throw new unauthenticated_1.default('InvalidToken', http_status_codes_1.StatusCodes.UNAUTHORIZED);
+    }
 }));
