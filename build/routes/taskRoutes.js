fda6e5a6b02a54a8c6f66ce69f9e95c6cdde97b7
beauspot <beauspot@outlook.com>
2023-06-08 18:24:26 +0100

    complied version of the auth route to javascript

--- build/routes/taskRoutes.js
+++ build/routes/taskRoutes.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const taskCtrls_1 = require("../controllers/taskCtrls");
+const authMiddleware_1 = require("../middleware/authMiddleware");
 const taskRoute = express_1.default.Router();
+taskRoute.use(authMiddleware_1.authMiddleware);
 taskRoute.get('/alltasks', taskCtrls_1.getAllTasks);
 taskRoute.post('/createtask', taskCtrls_1.createTask);
 taskRoute.get('/task/:id', taskCtrls_1.getASingleTask);
